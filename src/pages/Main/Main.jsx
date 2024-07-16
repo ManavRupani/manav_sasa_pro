ec4e9dad9655c88a5f2dd76d0e87e62e03f08f3f
Manav Rupani <manavasitsme@gmail.com>
2024-07-16 10:15:16 -0400

    all done

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainCard from '../component/card/MainCard';
+
+function Main() {
+  return (
+    <div>
+      This is home page
+      <MainCard />
+    </div>
+  );
+}
+
+export default Main
